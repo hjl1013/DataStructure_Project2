2694717b1efefa48643f5bea21199c85c1bd055a
hjl1013 <hjl1013@snu.ac.kr>
2022-11-27 02:47:37 +0900

    rotation, insertion implemented

--- AVLTree/avltree.hpp
+++ AVLTree/avltree.hpp
@@ -96,29 +96,115 @@ bool AVLTree<T,U>::remove(const T& key) {
 
 template<typename T, typename U>
 AVLNode<T,U>* AVLTree<T,U>::rotate_left(AVLNode<T,U>*& node){
-    //TODO    
+    //TODO
+    AVLNode<T,U>*& org_node = node;
+    AVLNode<T,U>*& org_right = node->right;
+    AVLNode<T,U>*& org_right_left = node->right->left;
 
+    // change structure
+    node = org_right;
+    node->left = org_node;
+    node->left->right = org_right_left;
 
+    // update heights
+    node->left->height = (node->left->left->height>node->left->right->height)? node->left->left->height+1: node->left->right->height+1;
+    node->height = (node->left->height>node->right->height)? node->left->height+1: node->right->height+1;
+
+    return node;
 }
 
 template<typename T, typename U>
 AVLNode<T,U>* AVLTree<T,U>::rotate_right(AVLNode<T,U>*& node){
     //TODO
+    AVLNode<T,U>*& org_node = node;
+    AVLNode<T,U>*& org_left = node->left;
+    AVLNode<T,U>*& org_left_right = node->left->right;
 
+    // change structure
+    node = org_left;
+    node->right = org_node;
+    node->right->left = org_left_right;
 
+    // update heights
+    node->right->height = (node->right->left->height>node->right->right->height)? node->right->left->height+1: node->right->right->height+1;
+    node->height = (node->left->height>node->right->height)? node->left->height+1: node->right->height+1;
+
+    return node;
 }
 
 template<typename T, typename U>
 AVLNode<T,U>* AVLTree<T,U>::insert(AVLNode<T,U>*& node, const T& key, const U& value) {
     //TODO
+    //End case
+    if (node == nullptr) {
+        node = new AVLNode<T, U>*&(key, value);
+    }
     
     //right-left
+    if (key > node->key && key < node->right->key) {
+        node->right->left = insert(node->right->left, key, value);
+
+        if (node->right->height - node->left->height > 1) {
+            AVLNode<T, U> *&org_node = node;
+            AVLNode<T, U> *&org_right = node->right;
+            AVLNode<T, U> *&org_right_left = node->right->left;
+
+            //change structure
+            org_node->right = org_right_left->left;
+            org_right->left = org_right_left->right;
+            node = org_right_left;
+            node->left = org_node;
+            node->right = org_right;
+
+            //update height
+            org_node->height = (org_node->left->height > org_node->right->height)? org_node->left->height+1: org_node->right->height+1;
+            org_right->height = (org_right->left->height > org_right->right->height)? org_right->left->height+1: org_right->right->height+1;
+            org_right_left->height = (org_right_left->left->height > org_right_left->right->height)? org_right_left->left->height+1: org_right_left->right->height+1;
+        }
+    }
 
     //right-right
-    
+    if (key > node->key && key > node->right->key) {
+        node->right->right = insert(node->right->right, key, value);
+
+        if (node->right->height - node->left->height > 1) {
+            node = rotate_right(node); //this updates height automatically
+        }
+    }
+
     //left-right
+    if (key < node->key && key > node->left->key) {
+        node->left->right = insert(node->left->right, key, value);
+
+        if (node->left->height - node->right->height > 1) {
+            AVLNode<T, U> *&org_node = node;
+            AVLNode<T, U> *&org_left = node->left;
+            AVLNode<T, U> *&org_left_right = node->left->right;
+
+            //change structure
+            org_node->left = org_left_right->right;
+            org_right->right = org_left_right->left;
+            node = org_left_right;
+            node->right = org_node;
+            node->left = org_left;
+
+            //update height
+            org_node->height = (org_node->left->height > org_node->right->height)? org_node->left->height+1: org_node->right->height+1;
+            org_left->height = (org_left->left->height > org_left->right->height)? org_left->left->height+1: org_left->right->height+1;
+            org_left_right->height = (org_left_right->left->height > org_left_right->right->height)? org_left_right->left->height+1: org_left_right->right->height+1;
+        }
+    }
    
-    //right-left
+    //left-left
+    if (key < node->key && key < node->left->key) {
+        node->left->left = insert(node->left->left, key, value);
+
+        if (node->left->height - node->right->height > 1) {
+            node = rotate_left(node); //this updates height automatically
+        }
+    }
+
+    return node;
 }
 
 template<typename T, typename U>
